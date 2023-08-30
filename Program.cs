79c667b4f10c0948721d8bd4058e728da5a8f6b6
phamnam91th <phamnam1991@gmail.com>
2023-08-30 10:11:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Supermarket_management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
